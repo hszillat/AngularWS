7e2be6801f5a00e9a1214c4452982dcf69729397
Holger Szillat <holger.szillat@pta.de>
2018-06-21 14:01:25 +0200

    Komponente fuer UserName Der Inhalt der username-Komponente in der app.component.html wird nach user.component.html transkludiert.

--- src/app/user/user/user.component.ts
+++ src/app/user/user/user.component.ts
@@ -1,13 +1,49 @@
-import { Component } from '@angular/core';
+import { UserNameComponent } from './user-name/user-name.component';
+import {
+  Component,
+  ViewEncapsulation,
+  ViewChild,
+  AfterViewInit,
+  QueryList,
+  ViewChildren,
+  ElementRef,
+  ContentChild,
+  AfterContentInit
+} from '@angular/core';
+import { UserAvatarComponent } from './user-avatar/user-avatar.component';
 
 @Component({
   selector: 'pta-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
+  // encapsulation: ViewEncapsulation.Emulated
 })
-export class UserComponent {
+export class UserComponent implements AfterViewInit, AfterContentInit {
+  /** Transkludierte Elemente */
+  @ContentChild(UserNameComponent)
+  userName: UserNameComponent;
+
+  /**
+   * Elemente aus Vorlage.
+   */
+  @ViewChild('btn') btn: ElementRef<HTMLButtonElement>;
+
+  @ViewChild(UserAvatarComponent) userAvatar: UserAvatarComponent;
+
+  @ViewChildren(UserAvatarComponent)
+  userAvatare: QueryList<UserAvatarComponent>;
+
   name = 'Holger Szillat';
   onClick() {
     this.name = 'angular rocks';
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.userAvatar, this.userAvatare.toArray());
+    console.log(this.btn);
+  }
+
+  ngAfterContentInit(): void {
+    console.log(this.userName);
   }
 }
