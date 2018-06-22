f13a885750a1b9f176a901add3c65ef9a13540b9
Holger Szillat <holger.szillat@pta.de>
2018-06-22 14:18:18 +0200

    Verwende deutsche locale
    Daten ausgeben mit Pipes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,19 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { NgModule, LOCALE_ID } from '@angular/core';
+import localeDE from '@angular/common/locales/de';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CommonUiModule } from './common-ui/common-ui.module';
 import { UserModule } from './user/user.module';
+import { registerLocaleData } from '@angular/common';
+
+registerLocaleData(localeDE);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    CommonUiModule,
-    UserModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  declarations: [AppComponent],
+  imports: [BrowserModule, CommonUiModule, UserModule, AppRoutingModule],
+  providers: [{ provide: LOCALE_ID, useValue: 'de' }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
